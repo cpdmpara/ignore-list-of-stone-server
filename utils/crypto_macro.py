4d2c651c6bc6751a203e49410faa59a0592d860b
가희 <leebadagyumin0129@gmail.com>
2025-10-23 09:53:35 +0900

    feat: 로그인용 비밀번호 hashing 함수 구현

---
+++ utils/crypto_macro.py
@@ -0,0 +1,21 @@
+from hashlib import sha512
+from dotenv import load_dotenv
+import os
+
+try:
+    load_dotenv("./.env")
+except:
+    pass
+
+SALT = os.getenv("HASH_SALT")
+INITAL_PASSWORD = os.getenv("INITAL_PASSWORD")
+
+def hash_func(password: str) -> bytes:
+    return sha512((password + SALT).encode()).digest()
+
+if __name__ == "__main__":
+    try:
+        with open("./admin_password.bin", "xb") as file:
+            file.write(hash_func(INITAL_PASSWORD))
+    except:
+        pass
